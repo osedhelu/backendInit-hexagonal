0e2d8aa25f91cf74f04e27355b4e3164ad7428c0
Oscar Herrera <osedhelu@gmail.com>
2023-11-25 01:23:18 -0500

    update(test-deployer):.git/hook

--- src/ioc.repository.ts
+++ src/ioc.repository.ts
@@ -1,9 +1,7 @@
 import { ContainerBuilder } from 'node-dependency-injection'
 import { UserModule } from './app/user/user.module'
-import { WhatsappModule } from './app/whatsapp/whatsapp.module'
 
 let container = new ContainerBuilder()
 UserModule(container)
-WhatsappModule(container)
 
 export default container
