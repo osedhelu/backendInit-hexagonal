82b40b049673a54f6bb6332f490b744edd967a45
Oscar Herrera <osedhelu@gmail.com>
2023-11-25 01:02:28 -0500

    update(init):emiminar directorios de pruebas

--- src/app/whatsapp/infrastructure/controllers/whatsapp.ctrl.ts
+++
@@ -1,9 +0,0 @@
-import { WhatsappCreate } from '@/app/whatsapp/application/whatsapp.create'
-
-export class whatsappCtrl {
-  constructor (private readonly whatsappCreator: WhatsappCreate) {}
-  public testCtrl = async (data: any) => {
-    return this.whatsappCreator.handle({ok: true})
-  }
-}
-
